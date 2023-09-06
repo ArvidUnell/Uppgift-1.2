884b90771865fc54df6896c6baf8ea631ac36293
arun06001 <arun06001@E-PW0234KE>
2023-09-06 12:48:54 +0200

    Add project files.

---
+++ Uppgift 1.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Uppgift1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            for (int i = 0; i < 7; i++) 
+            {
+                Console.WriteLine();
+            }
+            Console.WriteLine("                                  Windows");
+            Console.WriteLine();
+            Console.WriteLine("     An error has occured. To continue:");
+            Console.WriteLine();
+            Console.WriteLine("     Press Enter to return to Windows, or");
+            Console.WriteLine();
+            Console.WriteLine("     Press CTRL+ALT+DEL to restart your computer. If you do this");
+            Console.WriteLine("     you will lose any unsaved information in all open applications");
+            Console.WriteLine();
+            Console.WriteLine("     Error: 0E : 016F : BFF9B3D4");
+            Console.WriteLine();
+            Console.WriteLine("                         Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
